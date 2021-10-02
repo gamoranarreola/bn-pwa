5c1861775a5a321938fd76bd819c54ac1544709e
Guillermo A. Moran-Arreola <gmoran@bcdev.works>
2021-10-02 09:26:45 -0700

    fix client secret FB Oauth2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
     facebook: {
       client_id: 'EaHd1AmG4dFfy5g0qbbzsoqZaONsLayD6bY61aZv',
       // eslint-disable-next-line max-len
-      client_secret: 'EaHd1AmG4dFfy5g0qbbzsoqZaONsLayD6bY61aZv',
+      client_secret: 'T6r1bIzi5oFZjdlSwkJIJAysr5ohhe0Ums6HkWXHrQfY5hYWULstLlwBS0vCemBasEiUOGlnxaAlc8ULPE69KZubs2fle4px6iu0lc6Evt0UqPrse7P5l9USQHOwi8a3',
     },
   },
   ...defaultEnvironment,
