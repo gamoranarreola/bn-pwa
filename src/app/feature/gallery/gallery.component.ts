413ab63e4b635b89b980a3ad3d1ce2b74f9b4a05
devReneceo <dev@reneceo.com>
2022-08-08 17:38:03 -0700

    added new gallery slide

--- src/app/feature/gallery/gallery.component.ts
+++ src/app/feature/gallery/gallery.component.ts
@@ -12,6 +12,14 @@ export class GalleryComponent implements OnInit {
   images = [];
   images2 = [];
   images3 = [];
+  images4 = [];
+  images5 = [];
+  images6 = [];
+  images7 = [];
+  images8 = [];
+  images9 = [];
+  images10 = [];
+   images11 = [];
   slideOpts = {
     initialSlide: 1,
     speed: 400,
@@ -27,41 +35,48 @@ export class GalleryComponent implements OnInit {
   ngOnInit() {
 
     this.setImages(1,1)
-    this.setImages(10,1)
-    this.setImages(11,1)
+    this.setImages(10,2)
+    this.setImages(11,3)
    
 
-    this.setImages(12,2)
-    this.setImages(13,2)
-    this.setImages(14,2)
+    this.setImages(12,4)
+    this.setImages(13,5)
+    this.setImages(14,6)
 
-    this.setImages(15,3)
-    this.setImages(17,3)
-    this.setImages(7,3)
-    this.setImages(8,3)
+    this.setImages(15,7)
+    this.setImages(17,8)
+    this.setImages(7,9)
+    this.setImages(8,10)
+    this.setImages(14,11)
   }
 
   setImages(number, gallery) {
     this.apiDataService.getData(`${env.routes.beautiers.getBeautiers}/${number}/work`, false, 'get').subscribe((res: any)=>{   
-      if (gallery === 1) {
-        if (this.images.length === 0) { 
-          this.images = res.data}
-       else {
-        this.images.concat(res.data)
-       }
+      if (gallery === 1) {       
+          this.images = res.data
+       
       } else if (gallery === 2) {
-        if (this.images2.length === 0) { 
-          this.images2 = res.data}
-        else {
-         this.images2.concat(res.data)
-        }
-      }else if (gallery === 3) {
-        if (this.images3.length === 0) { 
-          this.images3 = res.data}
-       else {
-        this.images3.concat(res.data)
-       }
-      }
+        this.images2 = res.data
+        
+      }else if (gallery === 3) {        
+          this.images3 = res.data       
+      }else if (gallery === 4) {        
+        this.images4 = res.data       
+    }else if (gallery === 5) {        
+      this.images5 = res.data       
+  }else if (gallery === 6) {        
+    this.images6 = res.data       
+}else if (gallery === 7) {        
+  this.images7 = res.data       
+}else if (gallery === 8) {        
+  this.images8 = res.data       
+}else if (gallery === 9) {        
+  this.images9 = res.data       
+}else if (gallery === 10) {        
+  this.images10 = res.data       
+}else if (gallery === 11) {        
+  this.images11 = res.data       
+}
     });
   }
   openPreview(img) {
